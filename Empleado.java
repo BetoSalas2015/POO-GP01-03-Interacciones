fa5eb55d5b043ab4cb8482ee9e7dd20126f9e4f9
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-02-09 18:52:56 -0600

    Sesion 11 - 09/02/2022

---
+++ Empleado.java
@@ -0,0 +1,57 @@
+
+/**
+ * Write a description of class Empleado here.
+ * 
+ * @author Roberto Salazar Marquez
+ * @version 1.0
+ */
+import java.util.Date;
+
+public class Empleado
+{
+    private String nombre;
+    private String apellido;
+    private int numeroEmpleado;
+    private Date ingreso;
+    private double salario;
+    
+    public Empleado(String nom, String ap, double sal, int dia,int mes, int año)
+    {
+        nombre = new String(nom);
+        apellido = new String(ap);
+        numeroEmpleado = 100;
+        ingreso = new Date(año, mes, dia);
+        setSalario(sal);
+    }
+    
+    public void setSalario(double sal) {
+        salario = (sal > 0) ? sal : 1;
+    }
+    
+    public double getSalario() {
+        return salario;
+    }
+    
+    public String getNombre() {
+        return new String(nombre);              //  enfoque uno
+    }
+    
+    public String getApellido() {
+        return new String(apellido);        // enfoque dos
+    }
+    
+    public int getNumeroEmpleado() {
+        return numeroEmpleado;
+    }
+    
+    public Date getIngreso() {
+        return new Date(); 
+    }
+}
+
+
+
+
+
+
+
